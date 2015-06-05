4fbd3f9c137741f981e0061d595c1cbafb0ed354
Hai Li <hali@codeaurora.org>
2015-06-05 14:37:59 -0400

    modetest: add atomic and ion buf support in libdrm
    * Add atomic ioctl test option for all the
      settings and pageflip
    
      Usage: modetest -a
    
    * drmModeAtomicAddProperty returns error code with negative
      value. Fix the API return data check for follow valid error
      path.
    
    * Support ion buffer allocation instead of dumb buffer
     allocations in drm driver
    
    Change-Id: I781eac370a2b319f7381e5ede417377b1c79bb29

--- tests/modetest/Android.mk
+++ tests/modetest/Android.mk
@@ -5,6 +5,12 @@ include $(LOCAL_PATH)/Makefile.sources
 
 LOCAL_SRC_FILES := $(filter-out %.h,$(MODETEST_FILES))
 
+ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
+kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_C_INCLUDES              := $(kernel_includes)
+LOCAL_CFLAGS                  := $(common_flags) -DUSE_ION
+endif
+
 LOCAL_MODULE := modetest
 
 LOCAL_SHARED_LIBRARIES := libdrm
